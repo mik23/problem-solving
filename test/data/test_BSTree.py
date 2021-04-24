20f3f583c1e8720c28337b0599e5d76da465d197
Michael <marcotrigiano.m@gmail.com>
2021-04-24 19:33:21 +0200

    cleanup

--- test/data/test_BSTree.py
+++ test/data/test_BSTree.py
@@ -1,14 +1,11 @@
 from unittest import TestCase
-
 from binarytrees.data.BSTree import BSTree
 
 class Test(TestCase):
-
     def setUp(self):
-            #     4
+        #     4
         # 1			5
         #   3 
-
         self.tree = BSTree(4)
         self.tree.insert(1)
         self.tree.insert(3)
@@ -16,8 +13,6 @@ class Test(TestCase):
 
     def test_insertBSTree(self):
         self.assertEqual(self.tree.left.right.data, 3)
-        # self.tree.preOrderTraversing()
-
 
     def test_searchFoundBSTree(self):
         self.assertTrue(self.tree.search(1))
